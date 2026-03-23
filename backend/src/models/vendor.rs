1ffd6de08a370c34101ab581772a2cb0146671f0
0xsettings <bookdjspindim@gmail.com>
2026-03-23 20:19:50 +0100

    -- models

--- backend/src/models/vendor.rs
+++ backend/src/models/vendor.rs
@@ -1,8 +1,18 @@
-use serde::Deserialize;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
+use uuid::Uuid;
 
-#[derive(Debug, Deserialize, Clone, PartialEq, FromRow)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, FromRow)]
 pub struct Vendor {
-
+    pub id: Uuid,
+    pub business_name: String,
+    pub owner_name: String,
+    pub phone: String,
+    pub email: Option<String>,
+    pub cac_number: Option<String>,
+    pub commission_bp: i16,
+    pub is_approved: bool,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>
 }

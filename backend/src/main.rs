b2d0dabb7049fd2128833f46dc2f954e6e4d6462
0xsettings <bookdjspindim@gmail.com>
2026-03-23 02:38:08 +0100

    --main

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+
+mod dto;
+mod config;
+mod routes;
+mod db;
+mod errors;
+mod middleware;
+mod models;
+mod repository; 
+mod services;
+
+#[tokio::main]
+async fn main() {
+    
+    //env variable
+    let _ = dotenv();
 }

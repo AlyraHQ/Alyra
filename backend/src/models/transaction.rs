3b8ce9e4dad7ff0ef04528b591e797474be5b51e
0xsettings <bookdjspindim@gmail.com>
2026-03-24 01:35:01 +0100

    -- models

---
+++ backend/src/models/transaction.rs
@@ -0,0 +1,20 @@
+use bigdecimal::BigDecimal;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, FromRow, PartialEq, Clone)]
+pub struct Transaction {
+    pub id:                 Uuid,
+    pub user_id:            Uuid,
+    pub device_id:          Uuid,
+    pub amount_kobo:        i64,
+    pub units_purchased:    BigDecimal,
+    pub channel:            String,
+    pub status:             String,
+    pub interswitch_ref:    Option<String>,
+    pub token_id:           Uuid,
+    pub initiated_at:       DateTime<Utc>,
+    pub completed_at:       Option<DateTime<Utc>>
+}

71460c875067af6b8a0f8c9745020567c4fbf3c3
Novaa <preciousgem1803@gmail.com>
2026-03-24 11:04:48 +0100

    -- payment dto

---
+++ backend/src/dto/payment_dto.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+/// Frontend sends for payment intiation 
+/// Amount kobo in i64 50000 = 500Naira
+/// Channel - ussd-web-diaspora
+#[derive(Debug, Deserialize)]
+pub struct InitiatePaymentRequest {
+    pub device_id: Uuid,
+    pub amount_kobo: i64,              
+    pub channel: String,              
+}
+
+/// Backend sends resp back with Interswitch payment URL hook
+/// payment url is subjected to redirection && ref = interswitch ref
+#[derive(Debug, Serialize)]
+pub struct InitiatePaymentResponse {
+    pub transaction_id: Uuid,
+    pub payment_url: String,           
+    pub reference: String,             
+    pub amount_naira: f64,             
+}

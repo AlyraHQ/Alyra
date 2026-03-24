138327d887ef1a057e257748a4b4cedffd6d42ed
0xsettings <bookdjspindim@gmail.com>
2026-03-24 01:24:28 +0100

    -- models

---
+++ backend/src/models/solar_kit.rs
@@ -0,0 +1,18 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+use uuid::Uuid;
+
+
+#[derive(Debug, Serialize, Deserialize, FromRow, PartialEq, Clone)]
+pub struct SolarKit {
+    pub id:                     Uuid,
+    pub device_id:              Uuid,
+    pub kit_serial_number:      String,
+    pub battery_percent:        i16,    
+    pub daily_rate_kobo:        String,       
+    pub is_active:              bool,
+    pub next_payment_due:       Option<DateTime<Utc>>,
+    pub activated_at:           Option<DateTime<Utc>>,
+
+}

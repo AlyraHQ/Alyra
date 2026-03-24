70d63fb7ffc11e00813fce245855236a7f73b046
0xsettings <bookdjspindim@gmail.com>
2026-03-24 01:55:47 +0100

    -- models

---
+++ backend/src/models/alert.rs
@@ -0,0 +1,19 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+use uuid::Uuid;
+use bigdecimal::BigDecimal;
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone, FromRow)]
+pub struct Alert {
+    pub id:                 Uuid,
+    pub device_id:          Uuid,
+    pub user_id:            Uuid,
+    pub alert_type:         String,
+    pub message:            String,
+    pub sent_via:           String,
+    pub status:             String,
+    pub scheduled_at:       DateTime<Utc>,
+    pub sent_at:            Option<DateTime<Utc>>,
+    pub created_at:         DateTime<Utc>
+}

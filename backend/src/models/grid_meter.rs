9cc3a916fb278d7f0923faec345fa6d68c239a1b
0xsettings <bookdjspindim@gmail.com>
2026-03-24 01:10:04 +0100

    -- models

--- backend/src/models/grid_meter.rs
+++ backend/src/models/grid_meter.rs
@@ -1,13 +1,15 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 use uuid::Uuid;
+use bigdecimal::BigDecimal;
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone, FromRow)]
-pub struct Grid_Meter {
-    pub id:                     Uuid
-    pub device_id               UUID UNIQUE NOT NULL REFERENCES devices(id) ON DELETE CASCADE,
-    pub meter_number            VARCHAR(20) UNIQUE NOT NULL,
-    pub tariff_kobo_per_kwh     BIGINT NOT NULL,
-    pub units_balance           DECIMAL(12, 4) NOT NULL DEFAULT 0.0000,
-    pub last_vend_at   
+pub struct GridMeter {
+    pub id:                     Uuid,
+    pub device_id:              Uuid,
+    pub meter_number:           String,
+    pub tariff_kobo_per_kwh:    i64, 
+    pub units_balance:          BigDecimal, 
+    pub last_vend_at:           Option<DateTime<Utc>>
 }

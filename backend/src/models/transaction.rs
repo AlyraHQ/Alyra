92037f61518d89f875522068f9a7233616a92d9d
0xsettings <bookdjspindim@gmail.com>
2026-03-25 09:46:27 +0100

    -- txn model

--- backend/src/models/transaction.rs
+++ backend/src/models/transaction.rs
@@ -1,4 +1,5 @@
 
+use bigdecimal::BigDecimal;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
@@ -10,7 +11,7 @@ pub struct Transaction {
     pub user_id:            Uuid,
     pub device_id:          Uuid,
     pub amount_kobo:        i64,
-    pub units_purchased:    f64,
+    pub units_purchased:    BigDecimal,
     pub channel:            String,
     pub status:             String,
     pub interswitch_ref:    Option<String>,
